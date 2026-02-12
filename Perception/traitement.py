8880f1a15886049919615b3dcab252651e59dc0a
Rafaël HOFF <hoff.rafael06@gmail.com>
2026-02-12 13:20:37 +0100

    Ajout de la detection de contours (pour voir l'autre robot)

--- Perception/traitement.py
+++ Perception/traitement.py
@@ -17,7 +17,7 @@ def Aplatir(image):
     imglinit = cv.imread(image)
 
 
-def DetectObstacle(image):
+def DetectObstacle(image, color_lower, color_upper):
     hsv_img = cv.cvtColor(image, cv.COLOR_BGR2HSV)
     mask = cv.inRange(hsv_img, np.array(color_lower), np.array(color_upper))
 
@@ -54,18 +54,57 @@ def histogramme(image):
     res_img = cv.imdecode(img_arr, cv.IMREAD_COLOR)
     return res_img
 
+
+
+def contourDetection(img):
+    
+    # Conversion to grayscale 
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY) 
+
+    # Blurr
+    blur = cv.GaussianBlur(gray, (5,5), 0)
+    
+    # Find Canny edges 
+    edged = cv.Canny(blur, 30, 200) 
+    cv.imshow('Canny', edged) 
+
+    # Finding Contours 
+    contours, hierarchy = cv.findContours(edged, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) 
+    #cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE
+    #cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE
+    print("Number of Contours found = " + str(len(contours))) 
+    
+    # Draw all contours (i.e -1)
+    cv.drawContours(img, contours, -1, (0, 255, 0), 3) 
+    
+    cv.namedWindow('Contours', cv.WINDOW_NORMAL)  
+    cv.imshow('Contours', img)
+
 if __name__ == "__main__":
     cam = Camera()
     frame = cam.get_frame()
     
     if frame is not None:
+        # 1. Affichage de l'image originale
         cv.imshow('Frame', frame)
         
-        # Appel de la nouvelle fonction qui renvoie l'image
+        # 2. Test de l'Histogramme
         img_hist = histogramme(frame)
-        
-        # Affichage direct du résultat
         cv.imshow('Histogramme en direct', img_hist)
+        
+        # ---------------------------------------------------------
+        # 3. TEST DE DÉTECTION DE CONTOURS
+        # ---------------------------------------------------------
+        print("Lancement de la détection de contours...")
+        # On passe une copie pour ne pas gribouiller sur l'image originale
+        frame_contours = frame.copy()
+        contourDetection(frame_contours) 
+        
+        # 4. TEST DE DETECT OBSTACLE
+        lower_val = [0, 50, 150]
+        upper_val = [35, 200, 255]
+        mask_obstacle = DetectObstacle(frame, lower_val, upper_val)
+        cv.imshow('Masque Obstacle', mask_obstacle)
         
         cv.waitKey(0)
     
